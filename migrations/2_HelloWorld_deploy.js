5d0d1c4af5e45621024420985c89a576374f5f14
Byko <bykocrypto@gmail.com>
2020-07-23 23:44:09 +0200

    payable contract function

--- migrations/2_HelloWorld_deploy.js
+++ migrations/2_HelloWorld_deploy.js
@@ -1,8 +1,8 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts)  {
   deployer.deploy(HelloWorld).then(function(instance){
-    instance.setMessage("Hello Maki").then(function(){
+    instance.setMessage("Hello Maki", {value: 1000000, from: accounts[2]}).then(function(){
       instance.getMessage().then(function(message){
         console.log("Current message: " + message);
       })
