44c797a7472bfd9c6ea332ec90befba916da75db
Byko <bykocrypto@gmail.com>
2020-07-24 00:08:14 +0200

    error handling, test

--- migrations/2_HelloWorld_deploy.js
+++ migrations/2_HelloWorld_deploy.js
@@ -3,9 +3,13 @@ const HelloWorld = artifacts.require("HelloWorld");
 module.exports = function(deployer, network, accounts)  {
   deployer.deploy(HelloWorld).then(function(instance){
     instance.setMessage("Hello Maki", {value: 1000000, from: accounts[2]}).then(function(){
-      instance.getMessage().then(function(message){
-        console.log("Current message: " + message);
-      })
+     // instance.getMessage().then(function(message){
+     //   console.log("Current message: " + message);
+     console.log("Success");
+      }).catch(function(err){
+      console.log("error: " + err);
     });
-  });
+  }).catch(function(err){
+    console.log("Deploy failed " + err);
+  })
 };
