8fddb026ada1e8bf7ca8b5aa0e2f419ec74fee3f
Byko <bykocrypto@gmail.com>
2020-07-28 00:12:46 +0200

    unit test practice with mocha

--- test/helloWorldTest.js
+++ test/helloWorldTest.js
@@ -1,9 +1,9 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
 contract("HelloWorld", async function(){
-    instanceof("should initialize correctly", async function(){
+    it("should initialize correctly", async function(){
         let instance = await HelloWorld.deployed();
         let message = await instance.getMessage();
-        assert(message === "Hello Maki"); 
+        assert(message === "Hello Maki", "Message should be Hello Again!"); 
     });
 });
