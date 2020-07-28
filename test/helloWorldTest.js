1c59caa5626bcc51d1f1c265c8624a0bdd2f239d
Byko <bykocrypto@gmail.com>
2020-07-29 00:28:24 +0200

    multiple unit tests passed

--- test/helloWorldTest.js
+++ test/helloWorldTest.js
@@ -6,4 +6,11 @@ contract("HelloWorld", async function(){
         let message = await instance.getMessage();
         assert(message === "Hello Maki", "Message should be Hello Again!"); 
     });
+    it("should set the message correctly", async function(){
+        let instance = await HelloWorld.deployed();
+        await instance.setMessage("Testing Message");
+        let message = await instance.getMessage();
+        assert(message === "Testing Message",
+        "Message should be the same as we set it to");
+    });
 });
